46f0295d96f7f9397d1659df8fe74e8acf83c10f
Jens Lorek <jens.lorek@accso.de>
2019-08-14 12:43:18 +0200

    double input handler fix

--- Blake.Client/wwwroot/jsInterop.js
+++ Blake.Client/wwwroot/jsInterop.js
@@ -1,10 +1,10 @@
 window.snakeFunctions = {
     startInputHandler: function (razorPage) {
-        console.log("Starting input handler...")
+        console.log("Starting input handler...");
         document.addEventListener('keydown', function(event) {
             console.log("Detected keydown", event);
             razorPage.invokeMethodAsync("HandleInput", event.code)
-            .then(response => console.log(response))
+                .then(response => console.log(response));
         });
     }
 };
